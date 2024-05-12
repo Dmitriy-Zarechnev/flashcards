9d236344614970940a782aecb0e5533c7451a33c
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-12 18:47:19 +0300

    🟢M - card - added clsx | added parametr-center in story | checked

--- src/components/ui/card/Card.stories.ts
+++ src/components/ui/card/Card.stories.ts
@@ -3,8 +3,10 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Card } from './'
 
 const meta = {
-  argTypes: {},
   component: Card,
+  parameters: {
+    layout: 'centered',
+  },
   tags: ['autodocs'],
   title: 'Components/Card',
 } satisfies Meta<typeof Card>
--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -1,5 +1,7 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import { clsx } from 'clsx'
+
 import s from '@/components/ui/card/Card.module.scss'
 
 type CardProps<T extends ElementType = 'div'> = {
@@ -10,7 +12,7 @@ export const Card = <T extends ElementType = 'div'>(props: CardProps<T>) => {
   const { as: Component = 'div', children, className, ...rest } = props
 
   return (
-    <Component className={`${s.card} ${className}`} {...rest}>
+    <Component className={clsx(s.card, className)} {...rest}>
       {children}
     </Component>
   )
