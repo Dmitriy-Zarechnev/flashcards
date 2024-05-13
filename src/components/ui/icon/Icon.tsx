f6fa3c5626f51056d09423179e1cdbb57eb92c2d
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-13 13:23:47 +0300

    🟢M - Icon component - storybook added | boilerplate styles updated

--- src/components/ui/icon/Icon.tsx
+++ src/components/ui/icon/Icon.tsx
@@ -2,7 +2,6 @@ import iconsSprite from '../../../assets/icons/sprite_svg.svg'
 
 type IconProps = {
   className?: string
-  fill?: string
   height?: string
   iconId: string
   viewBox?: string
@@ -13,7 +12,6 @@ export const Icon = (props: IconProps) => {
   return (
     <svg
       className={props.className}
-      fill={props.fill || 'white'}
       height={props.height || '16'}
       viewBox={props.viewBox || '0 0 24 24'}
       width={props.width || '16'}
