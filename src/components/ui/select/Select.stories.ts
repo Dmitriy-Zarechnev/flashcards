3c2a3b58f088eb22be7ee2de32d55a4d83fe9ee2
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-14 18:28:35 +0300

    14.05 - select finished

---
+++ src/components/ui/select/Select.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SelectComp } from '@/components/ui/select'
+
+const meta = {
+  argTypes: {},
+  component: SelectComp,
+  tags: ['autodocs'],
+  title: 'Components/Select',
+} satisfies Meta<typeof SelectComp>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultSelect: Story = {
+  args: {
+    disabled: false,
+  },
+}
+
+export const DisabledSelect: Story = {
+  args: {
+    disabled: true,
+  },
+}
--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,45 +1,47 @@
-import { useState } from 'react'
-
-import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as Select from '@radix-ui/react-select'
 
 import s from './Select.module.scss'
+
+type OptionItems = {
+  id: number
+  text: string
+  value: string
+}
 
 type SelectCompProps = {
   disabled?: boolean
 }
 
 export const SelectComp = ({ disabled = false, ...rest }: SelectCompProps) => {
-  const [arrow, setArrow] = useState(true)
-
-  const changeArrowHandler = () => {
-    setArrow(!arrow)
-  }
+  const optionItems: OptionItems[] = [
+    { id: 1, text: 'Orange', value: 'orange' },
+    { id: 2, text: 'Apple', value: 'apple' },
+    { id: 3, text: 'Banana', value: 'banana' },
+  ]
 
   return (
     <>
       <div>Select-box</div>
       <Select.Root disabled={disabled} {...rest}>
-        <Select.Trigger className={s.SelectTrigger} onClick={changeArrowHandler}>
-          <Select.Value placeholder={'select-box'} />
-          <Select.Icon>{arrow ? <ChevronDownIcon /> : <ChevronUpIcon />}</Select.Icon>
+        <Select.Trigger className={s.SelectTrigger}>
+          <Select.Value placeholder={'default text'} />
+          <Select.Icon>
+            <ChevronDownIcon />
+          </Select.Icon>
         </Select.Trigger>
 
         <Select.Portal>
           <Select.Content className={s.SelectContent}>
             <Select.Viewport className={s.SelectViewport}>
               <Select.Group className={s.SelectGroup}>
-                <Select.Item className={s.SelectItem} value={'Banana'}>
-                  <Select.ItemText className={s.SelectItemText}>Banana</Select.ItemText>
-                </Select.Item>
-
-                <Select.Item className={s.SelectItem} value={'Orange'}>
-                  <Select.ItemText className={s.SelectItemText}>Orange</Select.ItemText>
-                </Select.Item>
-
-                <Select.Item className={s.SelectItem} value={'Apple'}>
-                  <Select.ItemText className={s.SelectItemText}>Apple</Select.ItemText>
-                </Select.Item>
+                {optionItems.map(el => {
+                  return (
+                    <Select.Item className={s.SelectItem} key={el.id} value={el.value}>
+                      <Select.ItemText className={s.SelectItemText}>{el.text}</Select.ItemText>
+                    </Select.Item>
+                  )
+                })}
               </Select.Group>
             </Select.Viewport>
           </Select.Content>
