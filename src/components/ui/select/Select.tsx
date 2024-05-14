968036fdbebe702850dbd51b43f17c5d6371c5c3
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-14 08:03:12 +0300

    14.05 - select - 1

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,53 +1,76 @@
-import { useState } from 'react'
-
 import { ChevronDownIcon } from '@radix-ui/react-icons'
 import * as Select from '@radix-ui/react-select'
 
 import s from './Select.module.scss'
 
-export const SelectComp = () => {
-  // Создаем состояние для хранения выбранного значения
-  const [selectedValue, setSelectedValue] = useState('')
+// export const SelectComp = () => {
+//   return (
+//     <Select.Root>
+//       <Select.Trigger className={s.SelectTrigger}>
+//         <Select.Value placeholder={'select-box'} />
+//         <Select.Icon>
+//           <ChevronDownIcon />
+//         </Select.Icon>
+//       </Select.Trigger>
+//
+//       <Select.Portal>
+//         <Select.Content>
+//           <Select.ScrollUpButton />
+//           <Select.Viewport>
+//             <Select.Group>
+//               <Select.Item value={'Banana'}>
+//                 <Select.ItemText>Banana</Select.ItemText>
+//               </Select.Item>
+//
+//               <Select.Item value={'Orange'}>
+//                 <Select.ItemText>Orange</Select.ItemText>
+//               </Select.Item>
+//
+//               <Select.Item value={'Apple'}>
+//                 <Select.ItemText>Apple</Select.ItemText>
+//               </Select.Item>
+//             </Select.Group>
+//           </Select.Viewport>
+//           <Select.ScrollDownButton />
+//           <Select.Arrow />
+//         </Select.Content>
+//       </Select.Portal>
+//     </Select.Root>
+//   )
+// }
 
+export const SelectComp = () => {
   return (
-    <Select.Root onValueChange={setSelectedValue} value={selectedValue}>
+    <Select.Root>
       <Select.Trigger className={s.SelectTrigger}>
-        <Select.Value placeholder={'Select-box'} />
-        <Select.Icon className={'SelectIcon'}>
+        <Select.Value placeholder={'select-box'} />
+        <Select.Icon className={s.SelectIcon}>
           <ChevronDownIcon />
         </Select.Icon>
       </Select.Trigger>
-      {/*<Select.Portal>*/}
-      <Select.Content className={'SelectContent'}>
-        {/*<Select.ScrollUpButton className={'SelectScrollButton'}>*/}
-        {/*  <ChevronUpIcon />*/}
-        {/*</Select.ScrollUpButton>*/}
-        <Select.Viewport className={'SelectViewport'}>
-          <Select.Group>
-            {/*<Select.Label>Здесь Label должен быть</Select.Label>*/}
-            <Select.Item value={'apple'}>Apple</Select.Item>
-            <Select.Item value={'banana'}>Banana</Select.Item>
-            <Select.Item value={'blueberry'}>Blueberry</Select.Item>
-            <Select.Item value={'grapes'}>Grapes</Select.Item>
-            <Select.Item value={'pineapple'}>Pineapple</Select.Item>
-          </Select.Group>
-        </Select.Viewport>
-        {/*<Select.ScrollDownButton className={'SelectScrollButton'}>*/}
-        {/*  <ChevronDownIcon />*/}
-        {/*</Select.ScrollDownButton>*/}
-      </Select.Content>
-      {/*</Select.Portal>*/}
+
+      <Select.Portal>
+        <Select.Content className={s.SelectContent}>
+          <Select.ScrollUpButton className={s.SelectScrollUpButton} />
+          <Select.Viewport className={s.SelectViewport}>
+            <Select.Group className={s.SelectGroup}>
+              <Select.Item className={s.SelectItem} value={'Banana'}>
+                <Select.ItemText className={s.SelectItemText}>Banana</Select.ItemText>
+              </Select.Item>
+
+              <Select.Item className={s.SelectItem} value={'Orange'}>
+                <Select.ItemText className={s.SelectItemText}>Orange</Select.ItemText>
+              </Select.Item>
+
+              <Select.Item className={s.SelectItem} value={'Apple'}>
+                <Select.ItemText className={s.SelectItemText}>Apple</Select.ItemText>
+              </Select.Item>
+            </Select.Group>
+          </Select.Viewport>
+          <Select.ScrollDownButton className={s.SelectScrollDownButton} />
+          <Select.Arrow className={s.SelectArrow} />
+        </Select.Content>
+      </Select.Portal>
     </Select.Root>
   )
 }
-
-// const SelectItem = React.forwardRef(({ children, ...props }, forwardedRef) => {
-//   return (
-//     <Select.Item {...props} ref={forwardedRef}>
-//       <Select.ItemText>{children}</Select.ItemText>
-//       <Select.ItemIndicator className={'SelectItemIndicator'}>
-//         <CheckIcon />
-//       </Select.ItemIndicator>
-//     </Select.Item>
-//   )
-// })
