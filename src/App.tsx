82611fd24032e1585b0bee5a3f2020c7dfbc0a73
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-14 15:24:45 +0300

    14.05 - select radix - 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { SelectComp } from '@/components/ui/select'
 export function App() {
   return (
     <div>
+      <div>
+        Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam architecto
+        asperiores aut deserunt eaque earum esse exercitationem inventore itaque labore magni
+        necessitatibus optio perferendis placeat, praesentium recusandae repudiandae similique
+        suscipit tempora veniam voluptas voluptatum? Ab animi ducimus earum, expedita minima nemo
+        quaerat sit? Assumenda eligendi error laborum pariatur sunt.
+      </div>
       <SelectComp />
     </div>
   )
--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -3,42 +3,6 @@ import * as Select from '@radix-ui/react-select'
 
 import s from './Select.module.scss'
 
-// export const SelectComp = () => {
-//   return (
-//     <Select.Root>
-//       <Select.Trigger className={s.SelectTrigger}>
-//         <Select.Value placeholder={'select-box'} />
-//         <Select.Icon>
-//           <ChevronDownIcon />
-//         </Select.Icon>
-//       </Select.Trigger>
-//
-//       <Select.Portal>
-//         <Select.Content>
-//           <Select.ScrollUpButton />
-//           <Select.Viewport>
-//             <Select.Group>
-//               <Select.Item value={'Banana'}>
-//                 <Select.ItemText>Banana</Select.ItemText>
-//               </Select.Item>
-//
-//               <Select.Item value={'Orange'}>
-//                 <Select.ItemText>Orange</Select.ItemText>
-//               </Select.Item>
-//
-//               <Select.Item value={'Apple'}>
-//                 <Select.ItemText>Apple</Select.ItemText>
-//               </Select.Item>
-//             </Select.Group>
-//           </Select.Viewport>
-//           <Select.ScrollDownButton />
-//           <Select.Arrow />
-//         </Select.Content>
-//       </Select.Portal>
-//     </Select.Root>
-//   )
-// }
-
 export const SelectComp = () => {
   return (
     <Select.Root>
