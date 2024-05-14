3030dc2d6971188b3f40713c0d778c23538d9438
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-14 17:07:04 +0300

    14.05 - select radix - 2

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,40 +1,50 @@
-import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
 import * as Select from '@radix-ui/react-select'
 
 import s from './Select.module.scss'
 
-export const SelectComp = () => {
+type SelectCompProps = {
+  disabled?: boolean
+}
+
+export const SelectComp = ({ disabled = false, ...rest }: SelectCompProps) => {
+  const [arrow, setArrow] = useState(true)
+
+  const changeArrowHandler = () => {
+    setArrow(!arrow)
+  }
+
   return (
-    <Select.Root>
-      <Select.Trigger className={s.SelectTrigger}>
-        <Select.Value placeholder={'select-box'} />
-        <Select.Icon className={s.SelectIcon}>
-          <ChevronDownIcon />
-        </Select.Icon>
-      </Select.Trigger>
+    <>
+      <div>Select-box</div>
+      <Select.Root disabled={disabled} {...rest}>
+        <Select.Trigger className={s.SelectTrigger} onClick={changeArrowHandler}>
+          <Select.Value placeholder={'select-box'} />
+          <Select.Icon>{arrow ? <ChevronDownIcon /> : <ChevronUpIcon />}</Select.Icon>
+        </Select.Trigger>
 
-      <Select.Portal>
-        <Select.Content className={s.SelectContent}>
-          <Select.ScrollUpButton className={s.SelectScrollUpButton} />
-          <Select.Viewport className={s.SelectViewport}>
-            <Select.Group className={s.SelectGroup}>
-              <Select.Item className={s.SelectItem} value={'Banana'}>
-                <Select.ItemText className={s.SelectItemText}>Banana</Select.ItemText>
-              </Select.Item>
+        <Select.Portal>
+          <Select.Content className={s.SelectContent}>
+            <Select.Viewport className={s.SelectViewport}>
+              <Select.Group className={s.SelectGroup}>
+                <Select.Item className={s.SelectItem} value={'Banana'}>
+                  <Select.ItemText className={s.SelectItemText}>Banana</Select.ItemText>
+                </Select.Item>
 
-              <Select.Item className={s.SelectItem} value={'Orange'}>
-                <Select.ItemText className={s.SelectItemText}>Orange</Select.ItemText>
-              </Select.Item>
+                <Select.Item className={s.SelectItem} value={'Orange'}>
+                  <Select.ItemText className={s.SelectItemText}>Orange</Select.ItemText>
+                </Select.Item>
 
-              <Select.Item className={s.SelectItem} value={'Apple'}>
-                <Select.ItemText className={s.SelectItemText}>Apple</Select.ItemText>
-              </Select.Item>
-            </Select.Group>
-          </Select.Viewport>
-          <Select.ScrollDownButton className={s.SelectScrollDownButton} />
-          <Select.Arrow className={s.SelectArrow} />
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+                <Select.Item className={s.SelectItem} value={'Apple'}>
+                  <Select.ItemText className={s.SelectItemText}>Apple</Select.ItemText>
+                </Select.Item>
+              </Select.Group>
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </>
   )
 }
