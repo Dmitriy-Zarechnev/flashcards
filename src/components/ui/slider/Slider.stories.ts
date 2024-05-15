e15803d308e34717b69c8c06306f12725eef090a
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-15 09:03:28 +0300

    15.05 - slider amending

--- src/components/ui/slider/Slider.stories.ts
+++ src/components/ui/slider/Slider.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { SliderComponent } from '@/components/ui/slider'
+import { SliderComponent } from './'
 
 const meta = {
   argTypes: {},
