3025bf1037e0b83d230cacc4b8cbb90559bf212d
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-15 14:31:33 +0300

    🟢M__Tabs__toCheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
+import { Tabs } from '@/components/ui/tabs'
+
+const tabsData = [
+  { title: 'Switcher', value: 'tab1' },
+  { title: 'Tab 2', value: 'tab2' },
+  { disabled: true, title: 'Tab 3', value: 'tab3' },
+]
+
 export function App() {
   return (
     <div
-      style={{
-        alignItems: 'center',
-        columnGap: '500px',
-        display: 'flex',
-        flexDirection: 'row',
-        height: '100vh',
-        justifyContent: 'center',
-        width: '100vw',
-      }}
+    // style={{
+    //   alignItems: 'center',
+    //   columnGap: '500px',
+    //   display: 'flex',
+    //   flexDirection: 'row',
+    //   height: '100vh',
+    //   justifyContent: 'center',
+    //   width: '100vw',
+    // }}
     >
-      💩💩💩
+      <Tabs.Root notFullWidth tabs={tabsData} />
     </div>
   )
 }
---
+++ src/components/ui/tabs/Tabs.stories.tsx
@@ -0,0 +1,63 @@
+import { TabType, Tabs } from '@/components/ui/tabs'
+import { TabsProps } from '@/components/ui/tabs/Tabs'
+import { Meta, StoryFn } from '@storybook/react'
+import { fn } from '@storybook/test'
+
+// Define tabs data
+const tabsData: TabType[] = [
+  { title: 'Tab 1', value: 'tab1' },
+  { title: 'Tab 2', value: 'tab2' },
+  { disabled: true, title: 'Tab 3', value: 'tab3' },
+]
+
+// Template for stories
+/** isWithContent пропс только для истории, чтобы отключить контент, при исользованнии компоненты, просто
+ * не нужно добавлять children */
+const Template: StoryFn<{ isWithContent: boolean } & TabsProps> = args => (
+  <Tabs.Root {...args}>
+    {args.isWithContent && (
+      <>
+        <Tabs.Content value={'tab1'}>Tab 1</Tabs.Content>
+        <Tabs.Content value={'tab2'}>Tab 2</Tabs.Content>
+        <Tabs.Content value={'tab3'}>Tab 2</Tabs.Content>
+      </>
+    )}
+  </Tabs.Root>
+)
+
+export default {
+  argTypes: {
+    notFullWidth: {
+      control: 'boolean',
+      description: 'Not full width tabs',
+    },
+  },
+  component: Template,
+  tags: ['autodocs'],
+  title: 'Components/Tabs',
+} as Meta
+
+// Default Tabs story
+export const Default = Template.bind({})
+Default.args = {
+  defaultValue: tabsData[0].value,
+  onValueChange: fn(),
+  tabs: tabsData,
+}
+
+export const NotFullWidth = Template.bind({})
+NotFullWidth.args = {
+  defaultValue: tabsData[0].value,
+  notFullWidth: true,
+  onValueChange: fn(),
+  tabs: tabsData,
+}
+
+// Controlled Tabs story
+export const WithContent = Template.bind({})
+WithContent.args = {
+  defaultValue: tabsData[0].value,
+  isWithContent: true,
+  onValueChange: fn(),
+  tabs: tabsData,
+}
