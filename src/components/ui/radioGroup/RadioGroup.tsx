68007b399d4843ae273933fd81128bc41205a11f
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-15 15:29:44 +0300

    15.05 - added map radioGroup

--- src/components/ui/radioGroup/RadioGroup.tsx
+++ src/components/ui/radioGroup/RadioGroup.tsx
@@ -3,33 +3,35 @@ import * as Radio from '@radix-ui/react-radio-group'
 
 import s from './RadioGroup.module.scss'
 
+type Options = {
+  id: string
+  label: string
+  value: string
+}
+
 type RadioGroupProps = {
   disabled?: boolean
 }
 
 export const RadioGroup = ({ disabled = false }: RadioGroupProps) => {
+  const options: Options[] = [
+    { id: '1', label: 'RadioGroup 1', value: 'item 1' },
+    { id: '2', label: 'RadioGroup 2', value: 'item 2' },
+    { id: '3', label: 'RadioGroup 3', value: 'item 3' },
+  ]
+
   return (
     <Radio.Root className={s.RadioRoot}>
-      <div className={s.RadioItemWrapper}>
-        <Radio.Item className={s.RadioItem} disabled={disabled} id={'item1'} value={'item 1'}>
-          <Radio.Indicator className={s.RadioIndicator} />
-        </Radio.Item>
-        <Typography.Body2 htmlFor={'item1'}>RadioGroup 1</Typography.Body2>
-      </div>
-
-      <div className={s.RadioItemWrapper}>
-        <Radio.Item className={s.RadioItem} disabled={disabled} id={'item2'} value={'item 2'}>
-          <Radio.Indicator className={s.RadioIndicator} />
-        </Radio.Item>
-        <Typography.Body2 htmlFor={'item2'}>RadioGroup 2</Typography.Body2>
-      </div>
-
-      <div className={s.RadioItemWrapper}>
-        <Radio.Item className={s.RadioItem} disabled={disabled} id={'item3'} value={'item 3'}>
-          <Radio.Indicator className={s.RadioIndicator} />
-        </Radio.Item>
-        <Typography.Body2 htmlFor={'item3'}>RadioGroup 3</Typography.Body2>
-      </div>
+      {options.map(el => {
+        return (
+          <div className={s.RadioItemWrapper} key={el.id}>
+            <Radio.Item className={s.RadioItem} disabled={disabled} id={el.id} value={el.value}>
+              <Radio.Indicator className={s.RadioIndicator} />
+            </Radio.Item>
+            <Typography.Body2 htmlFor={'el.id'}>{el.label}</Typography.Body2>
+          </div>
+        )
+      })}
     </Radio.Root>
   )
 }
