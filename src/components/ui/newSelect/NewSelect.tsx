a43b9eac5a21233a5bbb05d3b3fcfb1d0a8e013a
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-16 17:01:41 +0300

    16.05 - start - 2

--- src/components/ui/newSelect/NewSelect.tsx
+++ src/components/ui/newSelect/NewSelect.tsx
@@ -1,3 +1,26 @@
+import { Typography } from '@/components/ui/typography'
+import { Select } from '@headlessui/react'
+
+import s from '@/components/ui/select/Select.module.scss'
+
 export const NewSelect = () => {
-  return <div></div>
+  return (
+    <div>
+      <Typography.Body2 className={s.SelectLabel}>Select Label</Typography.Body2>
+      <Select className={s.SelectTrigger} data-focus data-hover>
+        <option className={s.SelectItem} value={'active'}>
+          Active
+        </option>
+        <option className={s.SelectItem} value={'paused'}>
+          Paused
+        </option>
+        <option className={s.SelectItem} value={'delayed'}>
+          Delayed
+        </option>
+        <option className={s.SelectItem} value={'canceled'}>
+          Canceled
+        </option>
+      </Select>
+    </div>
+  )
 }
