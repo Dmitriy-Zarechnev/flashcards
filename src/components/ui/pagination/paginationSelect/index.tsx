a9a8b14c4b5bf7d51ed8b3f4f24cdfb6a16cdec1
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-18 11:34:11 +0300

    🟢M__Pagination__toCheck

---
+++ src/components/ui/pagination/paginationSelect/index.tsx
@@ -0,0 +1,75 @@
+import { Icon } from '@/components/ui/icon'
+import { Typography } from '@/components/ui/typography'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { clsx } from 'clsx'
+
+import s from './PaginationSelect.module.scss'
+
+type OptionsType =
+  | { label: number; value: number }
+  | { label: number; value: string }
+  | { label: string; value: number }
+  | { label: string; value: string }
+
+type SelectProps = {
+  className?: string
+  currentValue: number | string
+  disabled?: boolean
+  fullWidth?: boolean
+  onChange: (id: number | string) => void
+  options: OptionsType[]
+  selectTitle?: string
+}
+
+export const PaginationSelect = ({
+  className,
+  currentValue,
+  disabled = false,
+  fullWidth = true,
+  onChange,
+  options,
+  selectTitle,
+}: SelectProps) => {
+  const currentLabel = options.filter(el => el.value === currentValue)[0].label
+
+  return (
+    <div>
+      <Typography.Body2 className={clsx(s.SelectLabel, { [s.disabled]: disabled })}>
+        {selectTitle}
+      </Typography.Body2>
+      <Listbox disabled={disabled} onChange={onChange} value={currentValue}>
+        {({ open }) => (
+          <div className={clsx(s.FatherRelative, fullWidth && s.fullWidth, className)}>
+            <ListboxButton className={clsx(s.SelectTrigger, fullWidth && s.fullWidth)}>
+              {currentLabel}
+              <Icon
+                height={'16px'}
+                iconId={open ? 'arrowUpOutline' : 'arrowDownOutline'}
+                width={'16px'}
+              />
+            </ListboxButton>
+            <ListboxOptions
+              {...(!fullWidth && { anchor: 'bottom' })}
+              className={clsx(s.SelectGroup, fullWidth && s.fullWidth)}
+            >
+              {options.map(el => (
+                <ListboxOption
+                  className={({ focus, selected }) =>
+                    clsx(s.SelectItem, {
+                      [s.focus]: focus,
+                      [s.selected]: selected,
+                    })
+                  }
+                  key={el.value}
+                  value={el.value}
+                >
+                  {el.label}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </div>
+        )}
+      </Listbox>
+    </div>
+  )
+}
