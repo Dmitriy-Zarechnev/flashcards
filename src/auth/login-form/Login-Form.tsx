ec5ff47793ee219a1cfb85286627fc2ada3429c1
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-19 13:26:48 +0300

    19.05 - validation - 1

--- src/auth/login-form/Login-Form.tsx
+++ src/auth/login-form/Login-Form.tsx
@@ -9,11 +9,13 @@ type FormValues = {
 }
 
 export const LoginForm = () => {
-  const { control, handleSubmit, register } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>()
 
-  const onSubmit = (data: FormValues) => {
-    console.log(data)
-  }
   const {
     field: { onChange, value },
   } = useController({
@@ -22,10 +24,35 @@ export const LoginForm = () => {
     name: 'rememberMe',
   })
 
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
+
+  console.log('errors: ', errors)
+
+  const emailRegex =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <Input {...register('email')} label={'email'} />
-      <Input {...register('password')} label={'password'} />
+      <Input
+        {...register('email', {
+          pattern: { message: 'Invalid email', value: emailRegex },
+          required: 'Email is required',
+        })}
+        error={errors.email?.message}
+        label={'email'}
+        type={'email'}
+      />
+      <Input
+        {...register('password', {
+          minLength: { message: 'Password has to be at least 3 characters long', value: 3 },
+          required: 'Password is required',
+        })}
+        error={errors.password?.message}
+        label={'password'}
+        type={'password'}
+      />
       <Checkbox checked={value} onChange={onChange}>
         rememberMe
       </Checkbox>
