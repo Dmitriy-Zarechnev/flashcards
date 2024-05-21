5821798d6129bcc83d86e6f4b33a7c291328612d
Dmitriy-Zarechnev <zarechnev5021@gmail.com>
2024-05-21 14:02:13 +0300

    21.05 - button refactor

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
-import { Icon } from '@/shared/components/icon'
+// import { Icon } from '@/shared/components/icon'
 import { Typography } from '@/shared/components/typography'
 import { clsx } from 'clsx'
 
@@ -9,8 +9,6 @@ import s from './Button.module.scss'
 type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   fullWidth?: boolean
-  img?: boolean
-  imgId?: string
   variant: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<T>
 
@@ -20,8 +18,6 @@ export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) 
     children,
     className,
     fullWidth,
-    img = false,
-    imgId = 'logOut',
     variant = 'primary',
     ...rest
   } = props
@@ -31,7 +27,6 @@ export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) 
       className={clsx(s.button, s[variant], fullWidth && s.fullWidth, className)}
       {...rest}
     >
-      {img && <Icon height={'16px'} iconId={imgId} width={'16px'} />}
       <Typography.Subtitle2>{children}</Typography.Subtitle2>
     </Component>
   )
