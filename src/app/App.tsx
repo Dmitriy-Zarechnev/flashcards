54c0fb1654feaf4eab1452d26ebc681f3a1b7511
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-06-01 15:57:13 +0300

    🟢M__Done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,21 +6,11 @@ import { Layout } from '@/shared'
 import { Router } from './router'
 
 export function App() {
-  const centerStyles = {
-    alignItems: 'center',
-    display: 'flex',
-    height: '100vh',
-    justifyContent: 'center',
-    width: '100vw',
-  }
-
   return (
-    <div style={centerStyles}>
-      <Provider store={store}>
-        <Layout>
-          <Router />
-        </Layout>
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <Layout>
+        <Router />
+      </Layout>
+    </Provider>
   )
 }
--- src/shared/ui/assembled/page-header/PageHeader.tsx
+++ src/shared/ui/assembled/page-header/PageHeader.tsx
@@ -20,21 +20,23 @@ type PageHeaderProps = {
 
 export const PageHeader = forwardRef<ElementRef<'header'>, PageHeaderProps>(({ isSingUp }, ref) => {
   return (
-    <header className={s.headerWrapper} ref={ref}>
-      <img alt={'Project Picture'} className={s.img} src={logo} />
-      {isSingUp ? (
-        <div className={s.profileInfo}>
-          <Typography.Subtitle1>{profile.name}</Typography.Subtitle1>
-          <DropdownProfile
-            email={profile.email}
-            name={profile.name}
-            photo={profile.photo}
-            photoDescription={profile.photoDescription}
-          />
-        </div>
-      ) : (
-        <Button variant={'secondary'}>Sing In</Button>
-      )}
+    <header className={s.header} ref={ref}>
+      <div className={s.container}>
+        <img alt={'Project Picture'} className={s.img} src={logo} />
+        {isSingUp ? (
+          <div className={s.profileInfo}>
+            <Typography.Subtitle1>{profile.name}</Typography.Subtitle1>
+            <DropdownProfile
+              email={profile.email}
+              name={profile.name}
+              photo={profile.photo}
+              photoDescription={profile.photoDescription}
+            />
+          </div>
+        ) : (
+          <Button variant={'secondary'}>Sing In</Button>
+        )}
+      </div>
     </header>
   )
 })
