45ff8710a6d8846ea8db09b0495fe314a2adf0db
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-06-01 15:59:31 +0300

    Revert "🟢M__Done"
    This reverts commit 54c0fb1654feaf4eab1452d26ebc681f3a1b7511.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,11 +6,21 @@ import { Layout } from '@/shared'
 import { Router } from './router'
 
 export function App() {
+  const centerStyles = {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh',
+    justifyContent: 'center',
+    width: '100vw',
+  }
+
   return (
-    <Provider store={store}>
-      <Layout>
-        <Router />
-      </Layout>
-    </Provider>
+    <div style={centerStyles}>
+      <Provider store={store}>
+        <Layout>
+          <Router />
+        </Layout>
+      </Provider>
+    </div>
   )
 }
