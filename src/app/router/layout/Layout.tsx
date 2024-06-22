f75814fb56f09c8841063d569c0681f7ecdf1849
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-06-22 11:36:17 +0300

    => routing - loader

--- src/app/router/layout/Layout.tsx
+++ src/app/router/layout/Layout.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from 'react-router-dom'
 
+import { InitLoader } from '@/app/router/InitLoader'
+import { useMeQuery } from '@/services'
 import { PageHeader } from '@/shared'
 
 import s from './Layout.module.scss'
 
 export const Layout = () => {
+  const { data, isLoading, isUninitialized } = useMeQuery()
+
   return (
     <div className={s.layout}>
-      <PageHeader />
-      <main>
-        <Outlet />
-      </main>
+      <PageHeader data={data} />
+      <main>{isLoading || isUninitialized ? <InitLoader /> : <Outlet />}</main>
     </div>
   )
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,5 @@ export * from './flow/auth.service'
 export * from './flow/cards.service'
 export * from './flow/decks.service'
 
+export type { AuthResponse } from './types/auth.types'
 export type { CreateCardArgs, Deck } from './types/decks.types'
